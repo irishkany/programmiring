5f5dcdbf9f6b7f7db070ad0a818754a470936c31
irishkany <144730046+irishkany@users.noreply.github.com>
2023-12-23 23:49:12 +0300

    Update vigenre.py

--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -1,28 +1,35 @@
+def next_symbol(ord_symbol: int, shift: int) -> str:
+    if (ord_symbol < 65) or ((ord_symbol > 90) and (ord_symbol < 97)) or (ord_symbol > 122) or (shift == 0):
+        return chr(ord_symbol)
+    if ord_symbol == 90:
+        return next_symbol(65, shift - 1)
+    if ord_symbol == 122:
+        return next_symbol(97, shift - 1)
+    return next_symbol(ord_symbol + 1, shift - 1)
+
+def last_symbol(ord_symbol: int, shift: int) -> str:
+    if (ord_symbol < 65) or ((ord_symbol > 90) and (ord_symbol < 97)) or (ord_symbol > 122) or (shift == 0):
+        return chr(ord_symbol)
+    if ord_symbol == 65:
+        return last_symbol(90, shift - 1)
+    if ord_symbol == 97:
+        return last_symbol(122, shift - 1)
+    return last_symbol(ord_symbol - 1, shift - 1)
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
+    ciphertext = ''
+    keyword = keyword.upper() * (len(plaintext) // len(keyword) + 1)
+    for i in range(len(plaintext)):
+        ciphertext += next_symbol(ord(plaintext[i]), (ord(keyword[i]) - 65) % 26)
     return ciphertext
 
-
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
-    plaintext = ""
-    # PUT YOUR CODE HERE
+    plaintext = ''
+    keyword = keyword.upper() * (len(ciphertext) // len(keyword) + 1)
+    for i in range(len(ciphertext)):
+        plaintext += last_symbol(ord(ciphertext[i]), (ord(keyword[i]) - 65) % 26)
     return plaintext
+
+if __name__ == '__main__':
+    print(encrypt_vigenere(input('Введите строку: '), input('Введите ключ: ')))
+    print(decrypt_vigenere(input('Введите строку: '), input('Введите ключ: ')))
